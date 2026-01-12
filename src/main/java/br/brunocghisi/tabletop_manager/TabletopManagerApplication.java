d480ef95dd3ec752dec3a9fbb62a60338e7c81fc
brunocghisi <brunocostaghisi@gmail.com>
2026-01-12 16:06:00 -0300

    TabletopManager boot-strap init

---
+++ src/main/java/br/brunocghisi/tabletop_manager/TabletopManagerApplication.java
@@ -0,0 +1,21 @@
+package br.brunocghisi.tabletop_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TabletopManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TabletopManagerApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
