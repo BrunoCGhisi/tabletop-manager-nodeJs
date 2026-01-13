e9d210a95c899743a98fe7cecebb95894e000ab5
brunocghisi <brunocostaghisi@gmail.com>
2026-01-13 13:17:28 -0300

    Organizando pastas back-end e front-end corretamente

---
+++ back-end/src/main/java/br/brunocghisi/tabletop_manager/Controllers/PlayerController.java
@@ -0,0 +1,29 @@
+package br.brunocghisi.tabletop_manager.Controllers;
+
+import br.brunocghisi.tabletop_manager.Model.Player;
+import br.brunocghisi.tabletop_manager.Repository.PlayerBase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+
+    @Autowired
+    private PlayerBase repository;
+
+    @GetMapping
+    public List<Player> getPlayers(){
+        return  repository.getPlayers();
+    }
+
+    @PostMapping
+    public String addPlayer(@RequestBody Player newPlayer){
+        repository.getPlayers().add(newPlayer);
+        return "Jogador" + newPlayer.getNome() + "criado com sucesso!";
+    }
+
+
+}
---
+++ back-end/src/test/java/brunocghisi/br/tabletop_manager/TabletopManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package brunocghisi.br.tabletop_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabletopManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
